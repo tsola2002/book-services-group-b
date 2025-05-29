b0e2535cf970d24c904f5266c3b7972a5b94058d
Shifatu Halimah Olamide <shifatuhalimah20@gmail.com>
2025-05-29 10:41:23 +0100

    Modified the bookcomposite

--- bookcomposite/src/main/java/com/tsola2002/bookcomposite/controller/BookCompositeController.java
+++ bookcomposite/src/main/java/com/tsola2002/bookcomposite/controller/BookCompositeController.java
@@ -3,13 +3,11 @@ package com.tsola2002.bookcomposite.controller;
 import com.tsola2002.bookcomposite.entity.BookComposite;
 import com.tsola2002.bookcomposite.service.BookCompositeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/bookcomposite")
+@CrossOrigin(origins = "http://localhost:5173")
 public class BookCompositeController {
 
     @Autowired
--- bookcomposite/src/main/java/com/tsola2002/bookcomposite/service/BookCompositeService.java
+++ bookcomposite/src/main/java/com/tsola2002/bookcomposite/service/BookCompositeService.java
@@ -2,7 +2,6 @@ package com.tsola2002.bookcomposite.service;
 
 import com.tsola2002.bookcomposite.entity.BookComposite;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,23 +14,17 @@ public class BookCompositeService {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
     public BookComposite getBookComposite(int bookId) {
-//        String bookUrl = "http://localhost:8081/api/v1/books/" + bookId;
-//        String reviewUrl = "http://localhost:8083/api/v1/ratings/" + bookId;
-        String recommendationUrl = "http://localhost:8086/api/v1/recommendations/" + bookId;
+//        String bookUrl = "http://localhost:8084/api/v1/books/" + bookId;
+        String reviewUrl = "https://reviewservice-to9o.onrender.com/api/v1/reviews/" + bookId;
+        String recommendationUrl = "https://recommendationservice-rlr1.onrender.com/api/v1/recommendations/" + bookId;
 
 //        Map<String, Object> book = restTemplate.getForObject(bookUrl, Map.class);
-//        List<Map<String, Object>> review = restTemplate.getForObject(reviewUrl, List.class);
+        List<Map<String, Object>> review = restTemplate.getForObject(reviewUrl, List.class);
         List<Map<String, Object>> recommendation = restTemplate.getForObject(recommendationUrl, List.class);
 
 //        BookComposite composite = new BookComposite(book, review, recommendation);
-        BookComposite composite = new BookComposite(null, recommendation);
-
-        // Save to MongoDB
-        mongoTemplate.save(composite);
+        BookComposite composite = new BookComposite(review, recommendation);
 
         return composite;
     }
